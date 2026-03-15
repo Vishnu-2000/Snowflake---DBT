6fd8b85f18cc3809724149a22d5c89ea1d57aa15
vishnu.vk2000.official@gmail.com <vishnu.vk2000.official@gmail.com>
2026-03-15 15:16:02 +0000

    station dimension model created

---
+++ models/station_dimension.sql
@@ -0,0 +1,14 @@
+WITH BIKE AS
+(
+    select
+    distinct
+    start_station_id,
+    start_station_name,
+    start_lat,
+    start_lng
+    from 
+    {{ source('demo', 'bike') }}
+    where RIDE_ID!='ride_id'
+)
+
+select * from bike
